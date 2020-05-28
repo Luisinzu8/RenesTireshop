95d7631f1ae871305b6442f166bf45cb0c267e96
Luis Inzunza <luisinzu8@luiss-mbp.home>
2020-05-27 21:41:02 -0700

    add react

---
+++ Sample.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+
+import 'react-calendar/dist/Calendar.css';
+
+export default class Sample extends Component {
+  state = {
+    value: new Date(),
+  }
+
+  onChange = value => this.setState({ value })
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className="Sample">
+        <header>
+          <h1>react-calendar sample page</h1>
+        </header>
+        <div className="Sample__container">
+          <main className="Sample__container__content">
+            <Calendar
+              onChange={this.onChange}
+              showWeekNumbers
+              value={value}
+            />
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
