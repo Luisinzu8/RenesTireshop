6e45871bc202fecb6681c625e4e6d99240c77dcb
Luis Inzunza <luisinzu08@gmail.com>
2020-07-26 16:18:17 -0700

    import Services component

--- tires/src/App.js
+++ tires/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../src/components/Header'
 import Slider from '../src/components/Slider'
 import Navigation from './components/Navigation'
+import Services from './components/Services'
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
        <Header  />
        <Slider imageOne={{backgroundImage: 'url(images/rene2.jpg)'}} title="Rene's tire service" />
+       <Services />
     </div>
   );
 }
