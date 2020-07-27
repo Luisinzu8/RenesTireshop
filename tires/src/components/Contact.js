12420b5debb44530a698a2a004253f0b2ef20ff5
yourhuckleberry <githuckleberry@gmail.com>
2020-07-27 08:53:44 -0700

    ISSUE:
    Contact Component
    is possible duplicated inside
    
    branch: statisticsBlock.

---
+++ tires/src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="text-center">
+            <div className="header-info-mobile">
+                <div className>
+                    <p><i className="icon icon-locate" /><a href="https://goo.gl/maps/nszHrtNd88KknHWFA">130 W. Walnut Ave. Perris, CA 92571</a></p>
+                    <p><i className="icon icon-phone" /><a href="tel:(951)436-1445">(951)436-1445</a></p>
+                    <p><i className="icon icon-clock" />Mon - Sat: 9:00 am – 5:00 pm</p>
+                    <p><i className="icon icon-email" /><a href="mailto:officeone@youremail.com">officeone@youremail.com</a></p>
+                </div>
+            </div>
+<hr />
+  <div id="react-container" />
+    <div id="like_button_container" />
+<hr />
+    <h2 className="h-lg">What We Do</h2>
+    <p className="info">We offer full tire repair &amp; maintenance</p>
+</div>
+    )
+}
+export default Contact
